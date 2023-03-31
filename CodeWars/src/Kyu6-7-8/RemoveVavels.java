57ef2f363e3a4faccedbdb94a69c038835036fb7
Selin Şahin <93709277+selinsahinn@users.noreply.github.com>
2023-03-31 11:27:30 +0300

    Update RemoveVavels.java

--- CodeWars/src/Kyu6-7-8/RemoveVavels.java
+++ CodeWars/src/Kyu6-7-8/RemoveVavels.java
@@ -4,11 +4,6 @@ public class RemoveVavels {
 
     public class Troll {
         public static String disemvowel(String str) {
-            // Code away...
-
-            // Plan 1 : Arrayliste alıp gereksizi sil
-            // Plan 2 : string için gerekliyi bul kullan
-            //String ans = str.replaceAll("[aeiouAEIOU]", "");
 
             return str.replaceAll("[aeiouAEIOU]", "");
 
